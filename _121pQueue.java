38adec1bc74aec1a981703775dcaedfe4a8077fe
ayanJaved <9307ayan@gmail.com>
2024-06-30 21:04:44 +0530

    final commit

---
+++ _121pQueue.java
@@ -0,0 +1,37 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class _121pQueue {
+    static class Student implements Comparable<Student>{
+        String name;
+        int rank;
+        public Student(String name , int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+        @Override
+        public int compareTo(Student s2){
+            return this.rank-s2.rank;
+        }
+    }
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        pq.add(3);
+        pq.add(4);
+        pq.add(1);
+        pq.add(7);
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());
+            pq.remove();
+        }
+        PriorityQueue<Student> pq2 = new PriorityQueue<>();
+        pq2.add(new Student("A",4));
+        pq2.add(new Student("B",5));
+        pq2.add(new Student("C",2));
+        pq2.add(new Student("D",12));
+        while(!pq2.isEmpty()){
+            System.out.println(pq2.peek().name+" -> "+pq2.peek().rank);
+            pq2.remove();
+        }
+    }
+}
